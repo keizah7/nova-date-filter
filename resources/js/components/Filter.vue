6bc2db3002799a2b8aa763c06c1022e1d6570ae2
keizah7 <murashka.arturas@gmail.com>
2020-09-16 00:19:56 +0300

    Date time picker in custom date filter component

--- resources/js/components/Filter.vue
+++ resources/js/components/Filter.vue
@@ -5,18 +5,21 @@
         </h3>
 
         <div class="p-2">
-            <select
-                :dusk="filter.name + '-filter-select'"
-                class="block w-full form-control-sm form-select"
+            <date-time-picker
+                class="w-full form-control form-input form-input-bordered"
+                dusk="date-filter"
+                name="date-filter"
+                autocomplete="off"
                 :value="value"
+                :alt-format="altFormat"
+                :date-format="dateFormat"
+                :placeholder="placeholder"
+                :enable-time="enableTime"
+                :enable-seconds="enableSeconds"
+                :first-day-of-week="firstDayOfWeek"
+                @input.prevent=""
                 @change="handleChange"
-            >
-                <option value="" selected>&mdash;</option>
-
-                <option v-for="option in filter.options" :value="option.value">
-                    {{ option.name }}
-                </option>
-            </select>
+            />
         </div>
     </div>
 </template>
@@ -35,25 +38,48 @@ export default {
     },
 
     methods: {
-        handleChange(event) {
+        handleChange(value) {
             this.$store.commit(`${this.resourceName}/updateFilterState`, {
                 filterClass: this.filterKey,
-                value: event.target.value,
-            })
-
-            this.$emit('change')
+                value,
+            });
+            this.$emit('change');
         },
     },
-
+    
     computed: {
         filter() {
             return this.$store.getters[`${this.resourceName}/getFilter`](
-                this.filterKey
-            )
+                this.filterKey,
+            );
         },
 
         value() {
-            return this.filter.currentValue
+            return this.filter.currentValue;
+        },
+
+        altFormat() {
+            return this.filter.options.altFormat || 'Y-m-d H:i';
+        },
+
+        dateFormat() {
+            return this.filter.options.altFormat || 'Y-m-d H:i';
+        },
+
+        placeholder() {
+            return this.filter.options.placeholder || this.__('Choose date');
+        },
+
+        enableTime() {
+            return this.filter.options.enableTime || true;
+        },
+
+        enableSeconds() {
+            return this.filter.options.enableSeconds || false;
+        },
+
+        firstDayOfWeek() {
+            return this.filter.options.firstDayOfWeek || 0;
         },
     },
 }
