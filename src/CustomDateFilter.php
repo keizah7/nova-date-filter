255dbaa8175ed2a6aa307b5028e648dcebea610c
keizah7 <murashka.arturas@gmail.com>
2020-09-17 21:39:19 +0300

    Date filter methods

--- src/CustomDateFilter.php
+++ src/CustomDateFilter.php
@@ -2,6 +2,7 @@
 
 namespace Keizah7\CustomDateFilter;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Laravel\Nova\Filters\Filter;
 
@@ -64,5 +65,113 @@ class CustomDateFilter extends Filter
                 'options' => array_merge($this->defaultData, $this->options(resolve(Request::class)))
             ]
         );
+    }
+
+    /**
+     * Filter resources by selected day, hour and minutes
+     *
+     * @param $query
+     * @param $field
+     * @param $value
+     * @return mixed
+     */
+    public function byTime($query, $field, $value)
+    {
+        $startOfMinute = Carbon::parse($value)->startOfMinute();
+        $endOfMinute = $startOfMinute->copy()->endofMinute();
+
+        return $query->where($field, '>=', $startOfMinute)->where($field, '<=', $endOfMinute);
+    }
+
+    /**
+     * Filter resources by selected day and hour
+     *
+     * @param $query
+     * @param $field
+     * @param $value
+     * @return mixed
+     */
+    public function byHour($query, $field, $value)
+    {
+        $startOfHour = Carbon::parse($value)->startOfHour();
+        $endOfHour = $startOfHour->copy()->endOfHour();
+
+        return $query->where($field, '>=', $startOfHour)->where($field, '<=', $endOfHour);
+    }
+
+    /**
+     * Filter resources from selected day and hour
+     *
+     * @param $query
+     * @param $field
+     * @param $value
+     * @return mixed
+     */
+    public function fromHour($query, $field, $value)
+    {
+        $startOfHour = Carbon::parse($value)->startOfHour();
+
+        return $query->where($field, '>=', $startOfHour);
+    }
+
+    /**
+     * Filter resources to selected day and hour
+     *
+     * @param $query
+     * @param $field
+     * @param $value
+     * @return mixed
+     */
+    public function toHour($query, $field, $value)
+    {
+        $startOfHour = Carbon::parse($value)->endOfHour();
+
+        return $query->where($field, '<=', $startOfHour);
+    }
+
+    /**
+     * Filter resources by selected day
+     *
+     * @param $query
+     * @param $field
+     * @param $value
+     * @return mixed
+     */
+    public function byDay($query, $field, $value)
+    {
+        $startOfDay = Carbon::parse($value)->startOfDay();
+        $endOfDay = $startOfDay->copy()->endOfDay();
+
+        return $query->where($field, '>=', $startOfDay)->where($field, '<=', $endOfDay);
+    }
+
+    /**
+     * Filter resources from selected day
+     *
+     * @param $query
+     * @param $field
+     * @param $value
+     * @return mixed
+     */
+    public function fromDay($query, $field, $value)
+    {
+        $startOfDay = Carbon::parse($value)->startOfDay();
+
+        return $query->where($field, '>=', $startOfDay);
+    }
+
+    /**
+     * Filter resources to selected day
+     *
+     * @param $query
+     * @param $field
+     * @param $value
+     * @return mixed
+     */
+    public function toDay($query, $field, $value)
+    {
+        $endOfDay = Carbon::parse($value)->endOfDay();
+
+        return $query->where($field, '<=', $endOfDay);
     }
 }
